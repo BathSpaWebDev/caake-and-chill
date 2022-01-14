fc1f5896fe4a96a178f2111388bb4dde1ba78861
Callum/Xavi <callummander@gmail.com>
2022-01-14 11:22:38 +0000

    created grid component

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { GridContent } from "./components/Grid";
+import "./grid.css";
+
+// import "./App.css";
+
+
+const App = () => {
+
+  return (
+    <main>
+      <GridContent />
+    </main>
+
+  );
+};
+
+export default App;
---
+++ my-app/src/components/Grid.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+
+export const GridContent = () => {
+
+const [moviesData, setMoviesData] = useState([]);
+
+  const getData = () => {
+    fetch("movies.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then(response => response.json())
+      .then(data => setMoviesData(data));
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+    return(
+        <div className="container">
+        {moviesData.map((data, index) => (
+          <div key={index}>
+            <h5>{data.movie}</h5>
+            <p>{data.year}</p>
+            <p>{data.price}</p>
+            <p>{data.stock}</p>
+            {data.genre.map((item, index) => (
+              <p key={index}>{item}</p>
+            ))}
+          </div>
+        ))}
+      </div>
+        
+    )
+}
